b97ad7473e7b17ee03989799e64d122b4ea0fc7f
Spock <372842848@qq.com>
2018-08-23 00:32:52 +0800

    build fixed

--- vue-music/build/webpack.base.conf.js
+++ vue-music/build/webpack.base.conf.js
@@ -28,7 +28,7 @@ module.exports = {
     path: config.build.assetsRoot,
     filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
+      ? './'
       : config.dev.assetsPublicPath
   },
   resolve: {
